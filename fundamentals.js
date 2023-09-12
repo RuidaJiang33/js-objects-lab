a8fb6654ae9185dcc4950545d69c29860993eb12
Lantena <772456648@qq.com>
2023-09-12 16:10:36 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,21 +1,28 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : {make:'Toyota', model:'Corolla', cc:'1.8'},
+    address : "3 Walkers Lane",
+    previous_owners: [
+      {name : "Pat Smith" , address : "1 Main Street"},
+      {name : "Sheila Dwyer" , address : "2 High Street"}
+    ],
+    type : {make:"Toyota", model:"Corolla", cc:"1.8"},
     registration : '201WD1058'
   };
   
-  console.log(aCar.owner + ' drives a ' + aCar.type.make);
+  console.log(aCar.owner + " drives a " + aCar.type.make);
 
   aCar.mileage = 10000;
 
   aCar.color = {
     exterior : "red",
     interior : {
-        texture : leather,
-        shade : cream
+        texture : "leather",
+        shade : "cream"
     }
   }
 
-  console.log('The mileage is' + aCar.mileage)
-  console.log('The exterior color is' + aCar.exterior)
-  console.log('The texture is' + aCar.interior.texture + ' And the shade is' + aCar.interior.shade)
+  console.log("The mileage is " + aCar.mileage)
+  console.log("The exterior color is " + aCar.color['exterior'])
+  console.log("The texture is " + aCar.color['interior']['texture'] + ", and the shade is " + aCar.color['interior']['shade'])
+
+  console.log("First owner : " + aCar.previous_owners[0]['name'] + " - " + aCar.previous_owners[0]['address'] )
