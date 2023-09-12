9a0ff447dc8304d27aecf98948af1af30da115b7
Lantena <772456648@qq.com>
2023-09-12 12:39:50 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -5,3 +5,17 @@ const aCar = {
   };
   
   console.log(aCar.owner + ' drives a ' + aCar.type.make);
+
+  aCar.mileage = 10000;
+
+  aCar.color = {
+    exterior : "red",
+    interior : {
+        texture : leather,
+        shade : cream
+    }
+  }
+
+  console.log('The mileage is' + aCar.mileage)
+  console.log('The exterior color is' + aCar.exterior)
+  console.log('The texture is' + aCar.interior.texture + ' And the shade is' + aCar.interior.shade)
