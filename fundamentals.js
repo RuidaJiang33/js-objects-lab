6d9a7dfe956756318c5493fdbfaa0e37c4cb8c6a
Lantena <772456648@qq.com>
2023-09-12 16:50:30 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -6,11 +6,14 @@ const aCar = {
       {name : "Sheila Dwyer" , address : "2 High Street"}
     ],
     type : {make:"Toyota", model:"Corolla", cc:"1.8"},
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration : '201WD1058'
   };
   
+  //Nested Objects
   console.log(aCar.owner + " drives a " + aCar.type.make);
 
+  //Dynamic properties
   aCar.mileage = 10000;
 
   aCar.color = {
@@ -25,4 +28,18 @@ const aCar = {
   console.log("The exterior color is " + aCar.color['exterior'])
   console.log("The texture is " + aCar.color['interior']['texture'] + ", and the shade is " + aCar.color['interior']['shade'])
 
-  console.log("First owner : " + aCar.previous_owners[0]['name'] + " - " + aCar.previous_owners[0]['address'] )
+  //Arrays
+  console.log("First owner : " + aCar.previous_owners[0].name + " - " + aCar.previous_owners[0].address )
+
+  //Looping/Iteration constructs
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+}
+
+  for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i].name)
+  } 
+
+  for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
